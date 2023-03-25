9d4ed34b7d9fb68a2e50ef0f5e706485fc0a093e
SiMinus <124517416+SiMinus@users.noreply.github.com>
2023-03-25 19:45:07 +0000

    Add files via upload

--- SoccerGame-master/UpdatePlayer.m
+++ SoccerGame-master/UpdatePlayer.m
@@ -1,8 +1,9 @@
-function [updatedPlayer, updatedBall] = UpdatePlayer(players, ball, indexOfPlayer, timeDelta, playerOriginalPosition)
+function [updatedPlayer, updatedBall] = UpdatePlayer(players, ball, indexOfPlayer, timeDelta, playerOriginalPosition,goalsTeam1,goalsTeam2)
 % Updates the player state and ball state by action rules
 % This is the function we can replace with different variants for
 % comparing different players
 global lastTeamOnBall;
+global fallBehindTeam;
 nAttributes = size(players{3},2);
 updatedPlayer = {[0 0],[1 0],players{3}};
 nPlayers=length(players{1});
@@ -11,10 +12,11 @@ kickBallSigma = 1/200;
 passBallSigma = 1/200;
 kickBallAcceleration = 1;
 passBallAcceleration = 1;
-shootBallCoefficient=9;
+shootBallCoefficient=7;
 passBallCoefficient=0.2;
 moveForwardCoefficient=0.5;
 markedDistance=12; %10-15 seems optimal
+threatedDistance=20;
 kickBallProbabilityCoefficient=25;
 actionBallDistance = 1.5;
 actionPlayerDistance = 36;
@@ -46,6 +48,18 @@ ballPosition = ball(1,:);
 distanceToBall = sqrt((ballPosition(1) - playerPosition(1))^2 + (ballPosition(2) - playerPosition(2))^2);
 distanceToGoal = sqrt((goalPosition(1) - playerPosition(1)).^2 + (goalPosition(2) - playerPosition(2)).^2);
 
+ if goalsTeam1 > goalsTeam2
+%      updatedPlayer = Attack(players, indexOfPlayer, updatedBall, timeDelta);
+   fallBehindTeam=1;
+ elseif goalsTeam1 < goalsTeam2
+   fallBehindTeam=0;
+ else
+     fallBehindTeam=NaN;
+ end
+
+ 
+
+
 if distanceToBall < actionBallDistance
     lastTeamOnBall=playerTeam;
     kickBallLikelihood=exp(-distanceToGoal/kickBallProbabilityCoefficient);
@@ -55,23 +69,33 @@ if distanceToBall < actionBallDistance
     passLikeRange = kickBallLikelihood + passBallLikelihood;
     
     if  whatTodo <= kickLikeRange || distanceToGoal < actionGoalDistance
+%         while IsThreated==true
+
+
         targetPosition = goalPosition;
         ball = KickBall(ball, kickBallSigma, shootBallCoefficient, kickBallAcceleration, targetPosition, timeDelta);
     elseif IsMarked(players,indexOfPlayer,playerTeam,markedDistance) || mod(indexOfPlayer,nPlayers/2)==0 %pass if you are the goalie
         targetPosition = ChoosePlayerToPass(players,indexOfPlayer,markedDistance);       
         ball = PassBall(ball, passBallSigma, passBallCoefficient, passBallAcceleration, targetPosition, timeDelta);
-    else % I am not marked and can go forward with the ball
+    else 
         targetPosition = [goalPosition(1)+sign(playerTeam-1/2) players{1}(indexOfPlayer,2)];
         ball = KickBall(ball, kickBallSigma, moveForwardCoefficient, kickBallAcceleration, targetPosition, timeDelta); 
     end
 end
 
 updatedBall = ball;
-updatedPlayer = Move(players, indexOfPlayer, updatedBall, timeDelta, playerOriginalPosition);
+% updatedPlayer = Move(players, indexOfPlayer, updatedBall, timeDelta, playerOriginalPosition);
+if (fallBehindTeam==playerTeam) && (indexOfPlayer==(1+playerTeam*nPlayers/2) || indexOfPlayer==(2+playerTeam*nPlayers/2))
+   updatedPlayer = Attack(players, indexOfPlayer, updatedBall, timeDelta);
+else
+   updatedPlayer = Move(players, indexOfPlayer, updatedBall, timeDelta, playerOriginalPosition);
+end
 
 end
 
-% NOTE: To make sure that the only file that we need to change to compare different PlayerAction.m files make sure to put all your helpfiles inside this function.
+% NOTE: To make sure that  the only file that we need to change to compare
+% different PlayerAction.m files make sure to put all your helpfiles inside
+% this function.
 
 
 
