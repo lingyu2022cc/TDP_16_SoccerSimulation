2ad0f86cdb76eaca7c18656bc4cfedb0c4b77383
kxl630 <124406952+kxl630@users.noreply.github.com>
2023-03-25 23:05:54 +0000

    Update KickBall.m

--- KickBall.m
+++ KickBall.m
@@ -1,17 +1,17 @@
 function [updatedBall] = KickBall(ball, kickBallSigma, kickBallCoefficient, kickBallAcceleration, targetPosition, timeDelta)
 
-% kickBallCoefficient 在updateplayer里面为 shootBallCoefficient=9;
+% kickBallCoefficient 脭脷updateplayer脌茂脙忙脦陋 shootBallCoefficient=9;
 
 ballPosition = ball(1,:);
 updatedBall = ball;
 %%set an error; error range:[-0.15,0.15]
 error = 0.3 * rand()- 0.15;
 
-distance_error = ballPosition - targetPosition;
+distance_error = targetPosition - ballPosition;
 distance = norm(distance_error);
 direction = distance_error/distance;
 
 kickDirection = direction + error;
-% 更新球的速度
+% 赂眉脨脗脟貌碌脛脣脵露脠
 updatedBall(2,:)= updatedBall(2,:)+kickBallCoefficient*kickDirection;
 end
