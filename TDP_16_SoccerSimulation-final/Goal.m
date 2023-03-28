e96c2846fb8662e765d71a3a8f4c2d6beb1377da
NighRdoda <117304304+NighRdoda@users.noreply.github.com>
2023-03-28 10:59:12 +0100

    Update Goal.m

--- TDP_16_SoccerSimulation-final/Goal.m
+++ TDP_16_SoccerSimulation-final/Goal.m
@@ -3,11 +3,11 @@ function [isGoal,goalsTeam1,goalsTeam2,kickoffTeam] = Goal(ball,goalsTeam1,goals
 isGoal=false;
 kickoffTeam=0;
 
-if ball(1,1)>= 45 && ball(1,2)>-30 && ball(1,2)<13
+if ball(1,1)>= 45 && ball(1,2)>-13 && ball(1,2)<13
     isGoal=true;
     goalsTeam1=goalsTeam1+1;
     kickoffTeam=1; %the other team will kickoff
-elseif ball(1,1)<=-45 && ball(1,2)>-30 && ball(1,2)<13
+elseif ball(1,1)<=-45 && ball(1,2)>-13 && ball(1,2)<13
     isGoal=true;
     goalsTeam2=goalsTeam2+1;
     kickoffTeam=0; %the other team will kickoff
