821999b19e1afff1e3c2dd284d5a8074e9e67c98
NighRdoda <117304304+NighRdoda@users.noreply.github.com>
2023-03-31 03:09:25 +0100

    Update KickBall.m

--- TDP_16_SoccerSimulation-final/KickBall.m
+++ TDP_16_SoccerSimulation-final/KickBall.m
@@ -1,18 +1,31 @@
 function [updatedBall] = KickBall(ball, kickBallSigma, kickBallCoefficient, kickBallAcceleration, targetPosition, timeDelta)
 
-% kickBallCoefficient= = 0.2 
-% shootBallCoefficient=9;
-
 ballPosition = ball(1,:);
 updatedBall = ball;
-%%set an error; error range:[-0.15,0.15]
-error = 0.5 * rand()- 0.15;
 
-distance_error = targetPosition - ballPosition;
-distance = norm(distance_error);
-direction = distance_error/distance;
+% Set an error; error range:[-0.15,0.15]
+error = 0.3 * rand() - 0.15;
 
+distanceError = targetPosition - ballPosition;
+distance = norm(distanceError);
+direction = distanceError / distance;
+
+% Add random noise to the kick direction
 kickDirection = direction + error;
+kickDirection(1) = kickDirection(1) + normrnd(0, kickBallSigma); % Add random noise to the x-component of the kick direction
+kickDirection(2) = kickDirection(2) + normrnd(0, kickBallSigma); % Add random noise to the y-component of the kick direction
 
-updatedBall(2,:)= updatedBall(2,:)+kickBallCoefficient*kickDirection;
+% Update the speed of the ball
+updatedBall(2,:) = updatedBall(2,:) + kickBallCoefficient * kickDirection;
+
+% Apply the kickBallAcceleration
+updatedBall(2,:) = updatedBall(2,:) + kickBallAcceleration * kickDirection;
+
+% Apply a damping factor to account for deceleration (e.g., due to air resistance or friction)
+dampingFactor = 0.95;
+updatedBall(2,:) = dampingFactor * updatedBall(2,:);
+
+% Update the position of the ball based on the updated velocity and timeDelta
+updatedBall(1,:) = updatedBall(1,:) + updatedBall(2,:) * timeDelta;
+
 end
