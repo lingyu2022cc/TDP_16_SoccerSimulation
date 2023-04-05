3df59dce230a26baefc0581aa17380e83b29149f
H.Yue <64911226+huyue0228@users.noreply.github.com>
2023-04-05 17:07:52 +0100

    Update PlayerMovement.m

--- TDP_16_RobocupSimulation-final/PlayerMovement.m
+++ TDP_16_RobocupSimulation-final/PlayerMovement.m
@@ -3,7 +3,8 @@ function [updatedPlayer] = PlayerMovement(players, indexOfPlayer, ball, timeDelt
 % This is the function where you define the movment behavoir for a player
 % This is an example of constant velocity while only chasing the ball
 % The velocity is set by the norm of the initial values in the xVel and yVel in the player matrix.
-
+a = 1;
+error = 1;
 nPlayers=length(players{1});
 playerTeam=players{3}(indexOfPlayer);
 actionPlayerDistance = 15; % 12-15 seems good
@@ -12,9 +13,6 @@ playerPosition = players{1}(indexOfPlayer,:);
 playerVelocity = players{2}(indexOfPlayer,:);
 playerPositions = players{1}(playerTeam*nPlayers/2+(1:nPlayers/2),:);
 
-%set the acceleration
-playerAcceleration = 1;
-
 ballPosition = ball(1,:);
 distanceToBall = norm(ballPosition-playerPosition);
 distanceToBallForAllTeamMates = vecnorm((ballPosition-playerPositions)');
@@ -22,24 +20,31 @@ distanceToBallForAllTeamMates = vecnorm((ballPosition-playerPositions)');
 distanceToOriginalPosition = norm(playerOriginalPosition(indexOfPlayer,:)-playerPosition);
 
 if indexOfPlayer==nPlayers/2 || indexOfPlayer==nPlayers %goalie
-    if (distanceToBall<actionPlayerDistance && distanceToOriginalPosition < actionPlayerDistance)...
+    if (distanceToBall < 1.5*actionPlayerDistance && distanceToOriginalPosition < 2*actionPlayerDistance && distanceToOriginalPosition > error)...
             || indexOfPlayer==(indexOfPlayerThatWillGoForTheBall+playerTeam*nPlayers/2)
         playerDirection = atan2(ballPosition(2) - playerPosition(2),ballPosition(1) - playerPosition(1));
+    elseif (distanceToOriginalPosition <= error)
+        a = 0;
+        playerDirection = 0;
     else
-        playerDirection = atan2(playerOriginalPosition(indexOfPlayer,2)- playerPosition(2),playerOriginalPosition(indexOfPlayer,1)- playerPosition(1));
+        playerDirection = atan2(playerOriginalPosition(indexOfPlayer,2) - playerPosition(2),playerOriginalPosition(indexOfPlayer,1) - playerPosition(1));
     end
+    
 else %players (not goalie)
-    if (distanceToBall < 1.0*actionPlayerDistance && distanceToOriginalPosition < 4.0*actionPlayerDistance)...
+    if (distanceToBall < 1.0*actionPlayerDistance && distanceToOriginalPosition < 4.0*actionPlayerDistance && distanceToOriginalPosition > error)...
             || indexOfPlayer==(indexOfPlayerThatWillGoForTheBall+playerTeam*nPlayers/2)
         playerDirection = atan2(ballPosition(2) - playerPosition(2),ballPosition(1) - playerPosition(1));
+    elseif (distanceToOriginalPosition <= error)
+        a = 0;
+        playerDirection = 0;
     else
-        playerDirection = atan2(playerOriginalPosition(indexOfPlayer,2)- playerPosition(2),playerOriginalPosition(indexOfPlayer,1)- playerPosition(1));
+        playerDirection = atan2(playerOriginalPosition(indexOfPlayer,2) - playerPosition(2),playerOriginalPosition(indexOfPlayer,1) - playerPosition(1));
     end
 end
 
-player{1}(indexOfPlayer,1) = playerPosition(1) + cos(playerDirection) * playerVelocity(1) * timeDelta;
-player{1}(indexOfPlayer,2) = playerPosition(2) + sin(playerDirection) * playerVelocity(1) * timeDelta;
-player{2}(indexOfPlayer,1) = 0 + playerAcceleration*timeDelta;
+player{1}(indexOfPlayer,1) = playerPosition(1) + a*cos(playerDirection) * playerVelocity(1) * timeDelta;
+player{1}(indexOfPlayer,2) = playerPosition(2) + a*sin(playerDirection) * playerVelocity(1) * timeDelta;
+player{2}(indexOfPlayer,1) = 1;
 player{2}(indexOfPlayer,2) = playerDirection;
 player{3} = players{3}(indexOfPlayer,:);
 
